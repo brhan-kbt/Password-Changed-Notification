34af2df09e5568dac9ed62a5417517651e7a832a
brhan-kbt <brhankbt63@gmail.com>
2024-09-19 13:17:49 +0300

    add new line between name and paragraph

--- resources/views/emails/password-changed-notification.blade.php
+++ resources/views/emails/password-changed-notification.blade.php
@@ -1,5 +1,7 @@
 <x-mail::message>
-    Hi, {{$user->getRawOriginal( $user->nameColumnName())}}
+Hi, {{$user->getRawOriginal( $user->nameColumnName())}}
+<br/>
+<br/>
 The password for your {{ config('app.name') }} account has been changed.
 Please contact  {{ config('app.name') }} support team if you have any questions.
 Thanks,<br>
